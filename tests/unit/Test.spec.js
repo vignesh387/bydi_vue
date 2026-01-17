31dc6f39c9737f470a9f9319b40c8c52498ad8fa
VigneshVijay <vignesh@shifteasy.com>
2026-01-17 11:55:19 +0400

    Unit test changes

--- tests/unit/Test.spec.js
+++ tests/unit/Test.spec.js
@@ -1,10 +1,12 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { mount } from "@vue/test-utils";
 import ErrorText from "../../src/components/ErrorText.vue";
 
 describe("Unit test error text", () => {
   it("Validate props", () => {
-    const wrapper = mount(ErrorText);
-    console.log("wrapper");
+    const wrapper = mount(ErrorText, {
+      props: { text: "username is required" },
+    });
+    expect(wrapper.text()).toBe("username is required");
   });
 });
